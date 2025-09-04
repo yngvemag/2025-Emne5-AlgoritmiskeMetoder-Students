6ddfe823ea25aeb63769e0d0f367c08403a85938
yngvemag <yngvem@gmail.com>
2025-09-04 11:40:53 +0200

    Added binary search

--- week36/CustomDataBinarySearch/Person.cs
+++ week36/CustomDataBinarySearch/Person.cs
@@ -1,8 +1,82 @@
 ﻿namespace CustomDataBinarySearch;
 
-public class Person
+public class Person : IComparable<Person>
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public int Age { get; set; }
+
+
+    // -1 -> this < other
+    // 0  -> this == other
+    // 1  -> this > other
+    public int CompareTo(Person? other)
+    {
+        if (other == null) return 1;
+        
+        int byLastName = 
+            string.Compare(LastName, other.LastName, StringComparison.OrdinalIgnoreCase);
+        
+        // If last names are different, return that comparison result
+        if (byLastName != 0)
+            return byLastName;
+        
+        // Last names are the same, compare by first name
+        int byFirstName = 
+            string.Compare(FirstName, other.FirstName, StringComparison.OrdinalIgnoreCase);
+        
+        // If first names are different, return that comparison result
+        if (byFirstName != 0)
+            return byFirstName;
+        
+        // Last and first names are the same, compare by age
+        return Age.CompareTo(other.Age);
+    }
+
+    public override string ToString() => $"FirstName: {FirstName}, LastName: {LastName}, Age: {Age}";
+}
+
+public class Person2
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+
+    public override string ToString() => $"FirstName: {FirstName}, LastName: {LastName}, Age: {Age}";
+}
+
+
+public class LastNameComparer : IComparer<Person2>
+{
+    public int Compare(Person2? x, Person2? y)
+    {
+        if (ReferenceEquals(x, y)) 
+            return 0;
+        
+        if (x == null) 
+            return -1;
+        
+        if (y == null) 
+            return 1;
+        
+        return string.Compare(x.LastName, y.LastName, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
+
+
+public class FirstNameComparer : IComparer<Person2>
+{
+    public int Compare(Person2? x, Person2? y)
+    {
+        if (ReferenceEquals(x, y)) 
+            return 0;
+        
+        if (x == null) 
+            return -1;
+        
+        if (y == null) 
+            return 1;
+        
+        return string.Compare(x.FirstName, y.FirstName, StringComparison.InvariantCultureIgnoreCase);
+    }
 }
--- week36/CustomDataBinarySearch/Program.cs
+++ week36/CustomDataBinarySearch/Program.cs
@@ -1,1 +1,70 @@
 ﻿
+using CustomDataBinarySearch;
+
+//TestPeople();
+TestPeople2();
+
+static void TestPeople()
+{
+    var people = new[]
+    {
+        new Person(){FirstName = "John", LastName = "Doe", Age = 30},
+        new Person(){FirstName = "Mary", LastName = "Hopper", Age = 50},
+        new Person(){FirstName = "Alan", LastName = "Turing", Age = 60},
+        new Person(){FirstName = "Bjarne", LastName = "Hansen", Age = 35},
+        new Person(){FirstName = "Gunn", LastName = "Olsen", Age = 24},
+    };
+
+    Console.WriteLine("Before sorting:");
+    foreach( var person in people )
+        Console.WriteLine(person);
+
+    Array.Sort(people);
+
+    Console.WriteLine("\nAfter sorting:");
+    foreach( var person in people )
+        Console.WriteLine(person);
+
+    int idx = Array.BinarySearch(
+        people,
+        new Person() {FirstName = "", LastName = "Hansen", Age = 0});
+    
+    Console.WriteLine(
+        idx >= 0
+            ? $"\nFound at index {idx}: {people[idx]}"
+            : "\nNot found"
+        );
+}
+
+static void TestPeople2()
+{
+    var people = new[]
+    {
+        new Person2(){FirstName = "John", LastName = "Doe", Age = 30},
+        new Person2(){FirstName = "Mary", LastName = "Hopper", Age = 50},
+        new Person2(){FirstName = "Alan", LastName = "Turing", Age = 60},
+        new Person2(){FirstName = "Bjarne", LastName = "Hansen", Age = 35},
+        new Person2(){FirstName = "Gunn", LastName = "Olsen", Age = 24},
+    };
+
+    Console.WriteLine("Before sorting:");
+    foreach( var person in people )
+        Console.WriteLine(person);
+
+    Array.Sort(people, new FirstNameComparer());
+
+    Console.WriteLine("\nAfter sorting:");
+    foreach( var person in people )
+        Console.WriteLine(person);
+    
+    int idx = Array.BinarySearch(
+        people,
+        new Person2() { LastName = "Hansen", FirstName = "" , Age = 0},
+        new LastNameComparer());
+    
+    Console.WriteLine(
+        idx >= 0
+            ? $"\nFound at index {idx}: {people[idx]}"
+            : "\nNot found"
+    );
+}
